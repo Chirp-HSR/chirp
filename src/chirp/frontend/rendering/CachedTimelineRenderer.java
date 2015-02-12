4d05286b3809fe49cef1adaa31e15b0ad5d981fa
Lukas Wegmann <wegmaluk@gmail.com>
2015-02-12 14:24:31 +0100

    add solution for in-memory cache

--- src/chirp/frontend/rendering/CachedTimelineRenderer.java
+++ src/chirp/frontend/rendering/CachedTimelineRenderer.java
@@ -1,11 +1,26 @@
 package chirp.frontend.rendering;
 
+import java.util.concurrent.TimeUnit;
+
 import chirp.api.Tweet;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
 public class CachedTimelineRenderer extends TimelineRenderer {
+	private final LoadingCache<Tweet, String> renderedTweets = CacheBuilder
+			.newBuilder().maximumSize(1000)
+			.expireAfterAccess(5, TimeUnit.MINUTES)
+			.build(new CacheLoader<Tweet, String>() {
+				@Override
+				public String load(Tweet tweet) {
+					return CachedTimelineRenderer.super.renderTweet(tweet);
+				}
+			});
+
 	@Override
 	public String renderTweet(Tweet tweet) {
-		// TODO Exercise 13: Implement in-memory caching
-		return super.renderTweet(tweet);
+		return renderedTweets.getUnchecked(tweet);
 	}
 }
